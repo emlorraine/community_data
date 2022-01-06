5be82fc0ef29fbab5bb08624c5ed45c9099bab22
emlorraine <52936908+emlorraine@users.noreply.github.com>
2022-01-06 12:26:34 -0600

    enabling jquery for comparisons

--- d3/js/load.js
+++ d3/js/load.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  console.log("page is ready")
-  $('.nav-tabs a[href="#intro"]').tab('show') 
-  
-  });
-
+  $('.nav-tabs a[href="#intro"]').tab('show')});
 
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
     var target = $(e.target).attr("href").substring(1) // activated tab
@@ -28,3 +24,60 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 
   });
 
+  $("#state-race").change(function(){
+    if ($(this).is(':checked'))
+      //Generate d3 here 
+      console.log("Generate race d3 state")
+      //Add div to race-block
+      $('#pills-race').append($("<div id='race-missouri'></div>"))
+  });
+  $("#city-race").change(function(){
+    if ($(this).is(':checked'))
+    console.log("Generate race d3 city")
+    //Add div to race-block
+    $('#pills-race').append($("<div id='race-city'></div>"))
+  });
+
+  $("#state-education").change(function(){
+    if ($(this).is(':checked'))
+      //Generate d3 here 
+      console.log("Generate education d3 state")
+      //Add div to education-block
+      $('#pills-education').append($("<div id='education-missouri'></div>"))
+
+  });
+  $("#city-education").change(function(){
+    if ($(this).is(':checked'))
+    console.log("Generate education d3 city")
+    //Add div to education-block
+    $('#pills-education').append($("<div id='education-city'></div>"))
+  });
+
+  $("#state-age").change(function(){
+    if ($(this).is(':checked'))
+      //Generate d3 here 
+      console.log("Generate age d3 state")
+      //Add div to age-block
+      $('#pills-age').append($("<div id='age-missouri'></div>"))
+
+  });
+  $("#city-age").change(function(){
+    if ($(this).is(':checked'))
+    console.log("Generate age d3 city")
+    //Add div to age-block
+    $('#pills-age').append($("<div id='age-city'></div>"))
+  });
+
+  $("#state-poverty").change(function(){
+    if ($(this).is(':checked'))
+      //Generate d3 here 
+      console.log("Generate poverty d3 state")
+      //Add div to poverty-block
+      $('#pills-poverty').append($("<div id='poverty-missouri'></div>"))
+  });
+  $("#city-poverty").change(function(){
+    if ($(this).is(':checked'))
+    console.log("Generate poverty d3 city")
+    //Add div to poverty-block
+    $('#pills-poverty').append($("<div id='poverty-city'></div>"))
+  });
