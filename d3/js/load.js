4553c734f2256b767ba142ed6dae9662e51c9e37
emlorraine <52936908+emlorraine@users.noreply.github.com>
2022-01-08 12:56:21 -0600

    Update load.js

--- d3/js/load.js
+++ d3/js/load.js
@@ -10,15 +10,19 @@ $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
      // Call datasets for branch:
     data2010FileName = "./data/" + target +"/2010/2010/Sheet1-Table 1.csv"
     data2020FileName = "./data/" + target +"/2019/2019/Sheet1-Table 1.csv"
-    Promise.all([
-        d3.csv(data2010FileName),
-        d3.csv(data2020FileName),
-        ]).then((values) => {
-            var raceBarChart = new RaceBarChart(values);
-            var educationalAttainmentChart = new EducationalAttainmentChart(values);
-            var ageChart = new AgeChart(values);
-            var povertyChart = new PovertyChart(values);          
-      });
+    if(d3.csv(data2010FileName) & d3.csv(data2020FileName)){
+      Promise.all([
+          d3.csv(data2010FileName),
+          d3.csv(data2020FileName),
+          ]).then((values) => {
+              var raceBarChart = new RaceBarChart(values);
+              var educationalAttainmentChart = new EducationalAttainmentChart(values);
+              var ageChart = new AgeChart(values);
+              var povertyChart = new PovertyChart(values);          
+        });
+    } else{
+      console.log("Can't find files:", data2020FileName, data2010FileName)
+    }
 
   });
 
