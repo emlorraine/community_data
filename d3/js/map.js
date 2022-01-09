3c10bb57b3074d74a12299d2d21c0b5a400f5dde
emlorraine <52936908+emlorraine@users.noreply.github.com>
2022-01-08 23:06:29 -0600

    data !!

--- d3/js/map.js
+++ d3/js/map.js
@@ -1,13 +1,43 @@
+
     var map = L.map('map',{
         center: [38.636975, -90.239971],
         zoom: 13
     });
-   
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZW1sb3JyYWluZSIsImEiOiJja25xcjFsanYwY3hjMm9uMHRkMmt1bzk3In0.AK4bzeFyxjafUrisldluWQ'
+    }).addTo(map);
+
+    // var promise = $.getJSON("../data/map/library_locations.json");
+    // promise.then(function(data) {
+    //     console.log(data)
+    //     var libraries = L.geoJson(data);
+    //     libraries.addTo(map);
+    //     console.log("added data to map")
+    // });
+
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(feature.properties['Branch Name']);
+      }
+
+    fetch("../data/map/library_locations.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        // use geoJSON
+        L.geoJSON(data, {
+        onEachFeature: onEachFeature,
+        }).addTo(map);
+    });
+    
+    // var marker = L.marker([38.624682, -90.185166]).addTo(map);
 
 
 
-    var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
-      }).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
-
-      
